d318d8cdc493e54c24f9a342298784d9d93644f7
myselfBZ <boburforfun@gmail.com>
2026-09-25 08:48:09 +0500

    order, user entities

--- src/entities.rs
+++ src/entities.rs
@@ -1,5 +1,30 @@
 use bigdecimal::BigDecimal;
+use chrono::NaiveDateTime;
 use serde::{Deserialize, Serialize};
+
+
+#[derive(Serialize)]
+pub struct User {
+    pub id: i64,
+    pub email: String,
+    pub name: String,
+}
+
+#[derive(Serialize)]
+pub struct OrderItem {
+    pub order_id: i64,
+    pub product_id: i64,
+    pub unit_price: BigDecimal,
+    pub quantity: i32
+}
+
+#[derive(Serialize)]
+pub struct Order {
+    pub id: i64,
+    pub user_id: i64,
+    pub created_at: NaiveDateTime,
+    pub items: Vec<OrderItem>
+}
 
 #[derive(Serialize, Deserialize)]
 pub struct Product {
